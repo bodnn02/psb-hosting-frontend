913accee300600727b3613e49aa050d9acde1a09
bodnn02 <bodnn03@gmail.com>
2023-12-02 13:05:22 +0200

    modified:   compontens/SystemList/SystemList.js

--- compontens/SystemList/SystemList.js
+++ compontens/SystemList/SystemList.js
@@ -38,7 +38,7 @@ const SystemList = ({ systems, selectedSystem: propSelectedSystem = null, onSele
       onSelect(system, selectedVersion && selectedVersion[system.id]);
     }
   };
-  
+
 
   const handleVersionClick = (version, systemId) => {
     setSelectedVersion((prev) => ({ ...prev, [systemId]: version }));
@@ -50,14 +50,14 @@ const SystemList = ({ systems, selectedSystem: propSelectedSystem = null, onSele
     setIsOpen((prev) => ({ ...prev, [systemId]: false }));
   };
 
-  var productSystems = systems.reduce((acc, os) => {
-    const osName = os.name.split(' ')[0].toLowerCase();
+  var productSystems = systems.os.reduce((acc, os) => {
+    const osName = os.name.split(/[-\s]/)[0].toLowerCase();
     const existingOS = acc.find(item => item.name === osName);
 
     if (existingOS) {
       existingOS.versions.push({
         id: os.id,
-        version: os.name.split(' ').slice(1).join(' '),
+        version: os.name.split(/[-\s]/).slice(1).join(' '),
         content: os.content
       });
       // Sort the versions array based on the version property
@@ -68,11 +68,12 @@ const SystemList = ({ systems, selectedSystem: propSelectedSystem = null, onSele
         name: osName,
         versions: [{
           id: os.id,
-          version: os.name.split(' ').slice(1).join(' '),
+          version: os.name.split(/[-\s]/).slice(1).join(' '),
           content: os.content
         }],
       });
     }
+
     return acc;
   }, []);
 
@@ -117,7 +118,7 @@ const SystemList = ({ systems, selectedSystem: propSelectedSystem = null, onSele
                 <div className={style["select-version"]} ref={(el) => (nodeRefs.current[system.id] = el)}>
                   <div className={style["select-version__header"]} onClick={() => toggleSelect(system.id)}>
                     <div className={style["select-version__title"]}>
-                    {(selectedVersion && selectedVersion[system.id] !== undefined && selectedVersion[system.id] !== null) ? selectedVersion[system.id] : t('new-service-system-version')}
+                      {(selectedVersion && selectedVersion[system.id] !== undefined && selectedVersion[system.id] !== null) ? selectedVersion[system.id] : t('new-service-system-version')}
                     </div>
                     <div className={style["select-version__arrow"]}></div>
                   </div>
